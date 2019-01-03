11da8b9b5ecb134235e2188a624917157e4fc2fb
Janaki <janakishreeja23@gmail.com>
2019-01-03 23:53:48 +0530

    commit

---
+++ printing_tokens.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    char *string;
+    int i=0;//i is index 
+
+    string=malloc(1000*sizeof(char));
+
+    printf("enter a string");
+    scanf("%[^\n]s",string);
+
+    while(*(string+i)!='\0')
+    {   
+        if(*(string+i)!=' ')//if not blank space print thr character
+        {
+        printf("%c",*(string+i));
+        i++;
+        }
+        else
+        {
+            printf("\n");
+            i++;
+        }
+    }
+        
+    
+}
+   
