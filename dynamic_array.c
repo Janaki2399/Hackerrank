4e886e43e63ef7f179d55cd821460e30b298c9f7
Janaki <janakishreeja23@gmail.com>
2019-01-03 19:14:10 +0530

    commit

---
+++ dynamic_array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+        int n,i;
+        int sum=0;
+
+        printf("enter the no of elements ");
+        scanf("%d",&n);
+        int *array =(int*)malloc(n * sizeof(int));//dynamically allocating memory for the array
+        
+        printf("enter the elements");
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",array+i);//storing the values in the address location
+
+        }
+        for(i=0;i<n;i++)
+        {
+            sum+=*(array+i);//calculating the sum using the values in that address location
+        }
+        printf("%d",sum);
+}
