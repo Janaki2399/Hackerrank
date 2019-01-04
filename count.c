aecb610e2809b06e090f1d34e7afcc3c4d9c852b
Janaki <janakishreeja23@gmail.com>
2019-01-04 14:35:29 +0530

    commit

---
+++ count.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char elements[50];
+    int index=0;
+    char number;
+    int count=0;
+    
+    printf("enter the elements");
+    scanf("%s",elements);
+
+    for(number='0';number<='9';number++)
+    {
+        while(elements[index]!='\0')
+        {
+             if(elements[index]==number)
+            {
+                count++;
+             }
+            index++;
+    
+        }
+        printf("%d",count);
+        count=0;
+        index=0;
+
+    }
+
+}
+        
