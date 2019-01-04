a9c3ee321a21da85df046462cae187d5436cd66b
janaki23 <janakishreeja23@gmail.com>
2019-01-05 00:09:32 +0530

    Update ConditionalStatements.c

--- ConditionalStatements.c
+++ ConditionalStatements.c
@@ -1,62 +1,17 @@
 #include<stdio.h>
-
-char* Englishword(int number)
-{
-    if(number==1)
-    {   
-        return "one";
-    }
-    else if(number==2)
-    {
-        return "two";
-    }
-    if(number==3)
-    {
-        return "three";
-    }
-    if(number==4)
-    {
-        return "four";
-    }
-    if(number==5)
-    {
-        return "five";
-    }
-    if(number==6)
-    {
-        return "six";
-    }
-    if(number==7)
-    {
-        return "seven";
-    }
-    if(number==8)
-    {
-        return "eight";
-    }
-    if(number==9)
-    {
-        return "nine";
-    }
-
-}
-
 int main()
 {
-    int number;
-    char *word;
-
-    printf("enter a number");
-    scanf("%d",&number);
-
-    if(number<=9)
+    int n;
+    char *words[]={"one","two","three","four","five","six","seven","eight","nine"};
+    scanf("%d",&n);
+    if(n<9)
     {
-        word=Englishword(number);
-        printf("%s",word);
+        printf("%s",words[n-1]);
     }
-    else
-    {
-        printf("number greater than 9");
+    else {
+     printf("Greater than 9");   
     }
 }
+
+
 
